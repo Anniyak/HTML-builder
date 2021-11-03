2d080ccf81970740afa912f971b55ee2ece682ba
Anniyak <57433322+Anniyak@users.noreply.github.com>
2021-11-03 16:05:26 +0300

    feat: 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const { stdin, stdout, exit } = process;
+const path = require('path');
+const fs = require('fs');
+
+stdout.write('Приветствую вас!\n');
+stdout.write('Введите текст для сохранения в файл:\n');
+stdin.on('data', text => {
+  if (text.toString() == 'exit\r\n') { process.exit(0); }
+  fs.appendFile(
+    path.join(__dirname, 'text.txt'),
+    text,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+});
+
+process.on('exit', code => {
+  if (code === 0) {
+    stdout.write('До новых встреч!');
+  } else {
+    stderr.write(`Произошла ошибка. Код ошибки ${code}`);
+  }
+});
+
+process.on('SIGINT', () => {
+  process.exit(0);
+});
+
